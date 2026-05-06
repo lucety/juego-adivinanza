7d834ca81b81250198c446d11375c2f5717f79f7
lucety <lucetyalman@gmail.com>
2026-05-06 14:03:04 -0500

    Versión 1.1: Juego básico con varios intentos

--- juego.py
+++ juego.py
@@ -1,14 +1,26 @@
 import random
-
+ 
 def jugar():
     print(" --- ¡Adivina el Número! --- ")
     numero_secreto = random.randint(1, 10)
-    intento = int(input("Elige un numero entre 1 y 10: "))
-
-    if intento == numero_secreto:
-        print("¡Felicidades! ¡Ganaste!")
-    else:
-        print(f"Perdiste. El numero era {numero_secreto}.")
-
-if __name__ == "__main__":
+    intentos =0
+ 
+    while True:
+        intento = int(input("Elige un número entre 1 y 10 (o 0 para salir): "))
+        intentos += 1
+ 
+        if intento == 0:
+            print("Te rendiste. Suerte para la proxima.")
+            break
+ 
+        if intento == numero_secreto:
+            print(f"¡Ganaste en {intentos} intentos!")
+            break
+        elif intento < numero_secreto:
+            print("El numero es mas alto.")
+        else:
+            print("El numero es mas bajo.")
+ 
+if __name__=="__main__":
     jugar()
+ 
